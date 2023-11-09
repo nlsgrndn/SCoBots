8678acc51ef116026a4d92da46895ae907d04706
nlsgrndn <nils.grandien@gmail.com>
2023-11-09 16:26:23 +0100

    update data_gatherer

--- src/engine/data_gatherer.py
+++ src/engine/data_gatherer.py
@@ -4,63 +4,21 @@ import gym
 from gym import wrappers, logger
 from rtpt import RTPT
 import matplotlib.pyplot as plt
-from gym.envs.classic_control import rendering
-from gym.wrappers.monitoring.video_recorder import VideoRecorder
+from spacetime_dqn import SpacetimeDQNAgent, model_name
+import torch
+from ocatari.core import OCAtari
+import rl_utils
+from gymnasium.wrappers.record_video import RecordVideo
 import multiprocessing as mp
 import os
 import numpy as np
 
-episode_count = 1000
+episode_count = 1
 # Create RTPT object
 rtpt = RTPT(name_initials='TRo', experiment_name='DataGatherer', max_iterations=episode_count)
 
 # Start the RTPT tracking
 rtpt.start()
-
-
-class GymRenderer():
-    def __init__(self, env, record=True, title="video"):
-        self.viewer = rendering.SimpleImageViewer()
-        self.env = env
-        self.record = record
-        if record:
-            self.video_rec = VideoRecorder(env.env, path=f"videos/{title}.mp4")
-            # create videos directory if it does not exist
-            if not os.path.exists("videos"):
-                os.makedirs("videos")
-            print("Videos will be stored in videos directory.")
-
-    def repeat_upsample(self, rgb_array, k=4, l=4, err=[]):
-        # repeat kinda crashes if k/l are zero
-        if rgb_array is None:
-            raise ValueError("The rgb_array is None, probably mushroom_rl bug")
-        if k <= 0 or l <= 0:
-            if not err:
-                print("Number of repeats must be larger than 0, k: {}, l: {}, returning default array!".format(k, l))
-                err.append('logged')
-            return rgb_array
-
-        # repeat the pixels k times along the y axis and l times along the x axis
-        # if the input image is of shape (m,n,3), the output image will be of shape (k*m, l*n, 3)
-
-        return np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)
-
-    def render(self, mode="zoomed"):
-        if self.record:
-            # self.env.render()
-            self.video_rec.capture_frame()
-        elif mode == "zoomed":
-            rgb = self.env.render('rgb_array')
-            upscaled = self.repeat_upsample(rgb, 4, 4)
-            self.viewer.imshow(upscaled)
-        else:
-            self.env.render()
-
-    def close_recorder(self):
-        if self.record:
-            self.video_rec.close()
-            self.video_rec.enabled = False
-
 
 class RandomAgent(object):
     """The world's simplest agent!"""
@@ -68,47 +26,68 @@ class RandomAgent(object):
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def act(self, observation, reward, done):
+    def act(self, state):
         return self.action_space.sample()
 
 
+
 def gather_agent(cfg, agent_id):
-    args_env_id = 'Pong-v0' #'SpaceInvaders-v0'
-    env = gym.make(args_env_id)
+    # load environment
+    args_env_id = 'Pong-v4'
+    env = OCAtari(args_env_id, mode="revised", hud=True, render_mode="rgb_array")
 
-    agent = RandomAgent(env.action_space)
-
-
-    reward = 0
-    done = False
+    #load agent
+    model_path = model_name(cfg.exp_name)
+    agent = SpacetimeDQNAgent(env.action_space.n, torch.device('cpu'),  model_path,)
+    space, transformation, sc, z_classifier = rl_utils.load_space(cfg)
+    use_cuda = "cuda" in cfg.device
 
     for i in range(episode_count):
-        ob = env.reset()
-        episode_id = agent_id * episode_count + i
-        renderer = GymRenderer(env, record = True, title=f'{args_env_id}_ep{episode_id:06}')
+
+        # reset environment
+        env.reset()
+        s_state = rl_utils.convert_to_stateOCAtari(cfg, env)
+        s_state = torch.tensor(s_state, dtype=torch.float) 
+        # state stacking to have current and previous state at once
+        state = torch.cat((s_state, s_state), 0)
+        env = RecordVideo(env, video_folder=f"videos")
+        env.start_video_recorder()
         step = 0
+
         while True:
-            action = agent.act(ob, reward, done)
-            ob, reward, done, _ = env.step(action)
-            renderer.render()
-            # plt.imshow(env.render('rgb_array'))
-            # plt.savefig(f"{out_dir}{args_env_id}_ep{agent_id*episode_count+i}_st{step}.png")
+            state = state.to(agent.device)
+            action = agent.act(state)
+            observation, reward, done, truncated, info = env.step(action.item())
+
+            # get state representation for next step
+            USE_GT_AS_STATE = False
+            if USE_GT_AS_STATE:
+                s_next_state = rl_utils.convert_to_stateOCAtari(cfg, env)
+            else:
+                _, s_next_state = rl_utils.get_scene(cfg, observation, space, z_classifier, sc, transformation, use_cuda)
+            s_next_state = torch.tensor(s_next_state, dtype=torch.float)
+            state = torch.cat((s_state, s_next_state), 0)
+            s_state = s_next_state # store for next step such that s_next_state can be overwritten
+
+            env.render()
             step += 1
-            if done:
+            print(step)
+            if done or step > 1000:
                 break
-        renderer.close_recorder()
+        env.close_video_recorder()
+        print(f"Episode {i} finished after {step} steps.")
         if agent_id == 0:
             rtpt.step(subtitle=f"step={i}/{episode_count}")
 
     env.close()
 
 
-NUM_PROCESSES = 16
+NUM_PROCESSES = 1
 
 
 def gather(cfg):
-    for agent_id in range(NUM_PROCESSES):
-        proc = mp.Process(target=gather_agent, args=(cfg, agent_id))
-        proc.start()
-
+    #for agent_id in range(NUM_PROCESSES):
+    #    proc = mp.Process(target=gather_agent, args=(cfg, agent_id))
+    #    proc.start()
+    print("ATTENTION: Currently only one episode can be gathered at a time.")
     gather_agent(cfg, NUM_PROCESSES)
