826ef2a09e665f7065d85de26c0628c56ce92b9a
nlsgrndn <nils.grandien@gmail.com>
2023-11-30 21:19:24 +0100

    try to fix z_what issue

--- src/create_labeled_data_for_classifier.py
+++ src/create_labeled_data_for_classifier.py
@@ -33,7 +33,6 @@ def retrieve_latent_repr_from_logs(logs):
     z_where, z_pres_prob, z_what = logs['z_where'], logs['z_pres_prob'], logs['z_what']
     z_where = z_where.detach().cpu()
     z_pres_prob = z_pres_prob.detach().cpu().squeeze()
-    z_what = z_what.detach().cpu()
     z_pres = z_pres_prob > 0.5
     return z_where, z_pres, z_pres_prob, z_what,
 
